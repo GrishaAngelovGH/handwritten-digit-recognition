5d7f04feaf51cafa574fecc7a611945aabefe8b5
Grisha Angelov <grisha.angelov89@gmail.com>
2022-07-28 09:20:40 +0300

    Minor grid improvements in DigitRecognition

--- src/components/DigitRecognition/DigitRecognition.js
+++ src/components/DigitRecognition/DigitRecognition.js
@@ -28,21 +28,21 @@ const DigitRecognition = () => {
     <div className="row g-0 justify-content-center">
       <h1 className="bg-primary text-white rounded p-2 text-center">Handwritten Digit Recognition with Tensorflow.js</h1>
 
-      <div className="col-md-10">
-        <div className="row">
-          <div className="col-md-4">
-            <Canvas width={150} height={150} onProcess={handleDataChange} onClear={handleClear} />
-          </div>
+      <div className="col-md-4">
+        <Canvas width={150} height={150} onProcess={handleDataChange} onClear={handleClear} />
+      </div>
 
-          <div className="col-md-8 bg-white rounded">
-            <BarChart data={data} />
-          </div>
+      <div className="col-md-7 bg-white rounded">
+        <BarChart data={data} />
+      </div>
 
+      <div className="row">
+        <div className="col-md-12">
           {message.length > 0 && (<h1 className="alert alert-success text-center mt-3">{message}</h1>)}
-
-          <ToastContainer />
         </div>
       </div>
+
+      <ToastContainer />
     </div>
   )
 }
